ecdf9577d0352c80e81b34385c635ef24ac00429
CharanTheCodeKID <156612608+CharanTheCodeKID@users.noreply.github.com>
2024-01-16 18:58:08 -0600

    Add files via upload

---
+++ Dup for It/views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template
+
+views = Blueprint(__name__, "view")
+
+@views.route("/")
+def home():
+    return render_template("home.html")
+
+@views.route("/about_us")
+def about_us():
+    return render_template("about_us.html")
+
+@views.route("/quiz/workout")
+def quiz():
+    return render_template("quiz.html")
+
+@views.route("/recipies")
+def common_recipies():
+    return render_template("Common Recipies.html")
+
+@views.route("/recipies/vegetarian")
+def common_recipies_vegetarian():
+    return render_template("Vegetarian.html")
